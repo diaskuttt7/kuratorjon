e47ec8d4ed0efed274e9d83bff76b8c5bffa5005
diaskuttt7 <roralov6@gmail.com>
2025-08-14 18:49:06 +0500

    Update matemqil.py

--- matemqil.py
+++ matemqil.py
@@ -9,7 +9,7 @@ import os
 from flask import Flask
 import threading
 
-channel_username = 'izatlox1'
+channel_username = 'kurator_kazino'
 
 accounts = [
     {"session": "account1", "api_id": 20262983, "api_hash": "d233b0bf40f861ce947ec5e95510300e", "message": "1346239969"},
@@ -115,3 +115,4 @@ def run_flask():
 if __name__ == "__main__":
     threading.Thread(target=run_flask).start()
     asyncio.run(telegram_main())
+
